95d3cc16cd80cefde0b4c584a3a9d1a0facd31b3
ENarayana <narayanaswamy2@email.com>
2026-01-06 21:46:14 +0530

    add of two sums

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,20 +26,20 @@ public class Main {
 
 
 
-//        int[] out = new int[3];
-//
-//        for (int i = 0; i < nums.length; i++) {
-//            for (int j = i + 1; j < nums.length; j++) {
-//                for (int k = j + 1; k < nums.length; k++) {
-//                    if (nums[i] + nums[j] + nums[k] == target) {
-//                        out[0] = i;
-//                        out[1] = j;
-//                        out[2] = k;
-//                        System.out.println("[" + out[0] + "," + out[1] + "," + out[2] + "]");
-//                        return;
-//                    }
-//                }
-//            }
-//        }
+        int[] out = new int[3];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == target) {
+                        out[0] = i;
+                        out[1] = j;
+                        out[2] = k;
+                        System.out.println("[" + out[0] + "," + out[1] + "," + out[2] + "]");
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
